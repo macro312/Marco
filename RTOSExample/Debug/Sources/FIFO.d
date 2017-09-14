e5f30307fbd63163b2a7c19498bd68a277532b92
Marco Pang <11424292@student.uts.edu.au>
2017-09-14 13:59:05 +1000

    FREESCALE MQX files
    GPIO example working.

---
+++ RTOSExample/Debug/Sources/FIFO.d
@@ -0,0 +1,20 @@
+Sources/FIFO.o: ../Sources/FIFO.c ../Sources/types.h ../Sources/FIFO.h \
+ C:/GIT_Repo/Marco/RTOSExample/Generated_Code/PE_Types.h \
+ C:/GIT_Repo/Marco/RTOSExample/Generated_Code/Cpu.h \
+ C:/GIT_Repo/Marco/RTOSExample/Generated_Code/PE_Types.h \
+ C:/GIT_Repo/Marco/RTOSExample/Generated_Code/IO_Map.h \
+ C:/GIT_Repo/Marco/RTOSExample/Static_Code/IO_Map/MK70F12.h
+
+../Sources/types.h:
+
+../Sources/FIFO.h:
+
+C:/GIT_Repo/Marco/RTOSExample/Generated_Code/PE_Types.h:
+
+C:/GIT_Repo/Marco/RTOSExample/Generated_Code/Cpu.h:
+
+C:/GIT_Repo/Marco/RTOSExample/Generated_Code/PE_Types.h:
+
+C:/GIT_Repo/Marco/RTOSExample/Generated_Code/IO_Map.h:
+
+C:/GIT_Repo/Marco/RTOSExample/Static_Code/IO_Map/MK70F12.h:
